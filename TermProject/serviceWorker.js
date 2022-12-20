b133550d836f1b50a4ddd2dafdf5b8833544dd81
ralkadamani <r.alkadamani@stu.fsm.edu.tr>
2022-12-20 12:03:29 +0300

    ad

--- TermProject/serviceWorker.js
+++ TermProject/serviceWorker.js
@@ -1,25 +1,41 @@
-const todolist = 'to-do-v1';
-// activate event
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key =>  key !== todolist)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-// fetch event
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(todolist).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          return fetchRes;
-        })
-      });
-    })
-  );
-});
+navigator.serviceWorker.register('serviceWorker.js')
+
+const CACHE ='JS'
+const FILES = ['/JS/', '/JS/sss/', '/JS/index.html', ...]
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+
+
+function cacheCB(e) { //cache first
+    let req = e.request
+    e.respondWith(
+      caches.match(req)
+      .then(r1 => r1 || fetch(req))
+      .catch(console.log)
+    )
+  }
+  self.addEventListener('fetch', cacheCB)
+
+  
+  function save(req, resp) {
+    return caches.open(CACHE)
+    .then(cache => {
+      cache.put(req, resp.clone());
+      return resp;
+    }) 
+    .catch(console.log)
+  }
+  function fetchCB(e) { //fetch first
+    let req = e.request
+    e.respondWith(
+      fetch(req).then(r2 => save(req, r2))
+      .catch(() => { return caches.match(req).then(r1 => r1) })
+    )
+  }
+  self.addEventListener('fetch', fetchCB)
